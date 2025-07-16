2bce78a156ffde6ea8b7555aee65f22c157c910a
Divash Krishnam <141182783+flashingCards@users.noreply.github.com>
2025-07-16 18:42:49 +0000

    array object and funcions

--- 01_basics/05_datatypeSummary.js
+++ 01_basics/05_datatypeSummary.js
@@ -3,3 +3,35 @@
 // premitive --> String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
 // refence(non premitive) --> array, objects, funcions
+
+const score = 100
+const scoreValue = 322.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherID = Symbol('123')
+
+console.log(id === anotherID)
+
+const binName = 4454548454541n // writing n at the end makes it big iint
+
+// this is an array
+const names = ["lallu", "mallu", "kallu"]
+
+// object
+let myObject = {
+    name: "lallu",
+    age: 22
+}
+
+const myFunction = function() {
+    console.log("hello world")
+}
+
+myFunction()
+console.log(typeof myFunction)
+// type of myFuncion is --> funcion which is object
+// --> OBJECT funcion or funciont OBJECT
