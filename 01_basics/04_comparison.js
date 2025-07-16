cd7771ffde67565bfc5c895020c131ece6d82e73
Divash Krishnam <141182783+flashingCards@users.noreply.github.com>
2025-07-16 15:52:05 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+console.log(2 > 1)
+console.log(2 == 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+// comparison converts the NULL into number 0
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+// === this check the data type as well
+
+console.log("2" === 2)
